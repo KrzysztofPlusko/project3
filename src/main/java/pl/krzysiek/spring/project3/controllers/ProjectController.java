15b8c63e00b4b9e41745ff2c701da38cb8b3b257
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-03-14 09:34:45 +0100

    Project Controller - display form

---
+++ src/main/java/pl/krzysiek/spring/project3/controllers/ProjectController.java
@@ -0,0 +1,20 @@
+package pl.krzysiek.spring.project3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.krzysiek.spring.project3.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    public String displayProjectForm(Model model) {
+
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+
+        return "/projects/new-project";
+
+    }
+}
