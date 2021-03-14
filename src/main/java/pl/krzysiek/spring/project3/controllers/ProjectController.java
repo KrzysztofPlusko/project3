4f150d5438ec9a03b6e19f79d2cafd056be034f0
Krzysztof Plusko <krzysiek.plus1@wp.pl>
2021-03-14 09:57:59 +0100

    porject controlller save project

--- src/main/java/pl/krzysiek/spring/project3/controllers/ProjectController.java
+++ src/main/java/pl/krzysiek/spring/project3/controllers/ProjectController.java
@@ -1,15 +1,20 @@
 package pl.krzysiek.spring.project3.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.krzysiek.spring.project3.entities.Project;
+import pl.krzysiek.spring.project3.repositories.IProjectRepository;
 
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+    @Autowired
+    IProjectRepository proRepo;
 
     @GetMapping("/new")
     public String displayProjectForm(Model model) {
@@ -25,8 +30,8 @@ public class ProjectController {
 
     public String createProject(Model Model, Project project) {
 
-        //ssssss
+        proRepo.save(project);
 
-        return"";
+        return"redirect:/projects/new-project";
     }
 }
